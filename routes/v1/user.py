7a4adf73a3db27b4af69089ccd12fc16cb1d3fc7
Edem Tsalah <edem.tsalah@gmail.com>
2018-10-20 06:52:37 +0000

    fixed the login route so that when no username and/or password are provided a useful error message is given to the user with the appropriate status code. Also added and tested the update of a users details apart from their password

--- routes/v1/user.py
+++ routes/v1/user.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 from datetime import datetime
 from sanic import Blueprint
@@ -11,8 +12,8 @@ from helpers import password_helper
 from helpers import jwt_helper
 from decorations.session_helper import inject_session
 from decorations.authentication_helper import authenticate
-user_bp = Blueprint('user_blue_print', url_prefix='/v1/user')
 
+user_bp = Blueprint('user_blue_print', url_prefix='/v1/user')
 
 
 @user_bp.get('/')
@@ -49,6 +50,14 @@ async def login(request, session_obj):
     if 'password' in params:
         params.update({'password': password_helper.encrypt(params['password'])})
 
+    # TODO: replace this loop when you integrate WTForms
+    for field in ('username', 'password'):
+        if field not in params:
+            return json({
+                'message': (
+                    'You need to provide your `{0}` to '
+                    'successfully log in'.format(field))
+                }, 401)
     args = [
         {"username": {"$eq": params['username']}},
         {"password": {"$eq": params['password']}}
@@ -74,4 +83,23 @@ async def find_user(request, session_obj, user_obj, user_id):
 async def count(request, session_obj, user_obj):
     params = param_helper.get_json(request, remove_token=True)
     result = query_helper.count(session_obj, User, params)
+    return json(result)
+
+
+@user_bp.put('/')
+@inject_session()
+@authenticate()
+async def update_user(request, session_obj, user_obj):
+    params = param_helper.get_json(request, remove_token=True)
+    filter_args = params.get('filters', [])  # TODO: check if version is passed
+    filter_args.append({'id': {'$eq': user_obj['id']}})
+    data = {'updated_at': datetime.utcnow(), 'ver': id_helper.generate_id()}
+    for field in params.keys():
+        if field not in (
+            'password', 'id', 'created_at', 'updated_at', 'ver', 'filters',
+            'limit', 'offset', 'sort'):
+            data.update({field: params[field]})
+    result = command_helper.update_by_params(
+        session_obj, User, filter_args, data, json_result=True)
+    session_obj.commit()
     return json(result)

0431e5b9d7e641be52f623390e5f6faef2b75d15
Edem Tsalah <edem.tsalah@gmail.com>
2018-10-21 13:47:37 +0000

    added decorator for handling common exception that might occur in the endpoint

---
+++ decorations/exception_helper.py
@@ -0,0 +1,38 @@
+import os
+from functools import wraps
+from helpers import jwt_helper
+from helpers import param_helper
+from sanic.response import json
+from jwt.exceptions import ExpiredSignatureError, DecodeError
+from sqlalchemy.exc import IntegrityError
+
+
+def rollback(session_obj):
+	if session_obj:
+		session_obj.rollback()
+
+
+def handle_exception():
+
+    def decorator(f):
+
+        @wraps(f)
+        async def decorated_function(request, *args, **kwargs):
+        	session_obj = None
+        	
+        	if 'session_obj' in kwargs:
+        		session_obj = kwargs['session_obj']
+
+        	try:
+        		return await f(request, *args, **kwargs)
+        	except IntegrityError:
+        		rollback(session_obj)
+        		return json({
+        			'message': (
+        				'You have to make sure that entity you are creating '
+        				'is unique')
+        			}, 400)
+
+        return decorated_function
+
+    return decorator

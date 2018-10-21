9df0d8cef4c25234d9d8ae496bd89991077d8a55
Edem Tsalah <edem.tsalah@gmail.com>
2018-10-21 20:54:29 +0000

    change the indentation from tabs to spaces

--- decorations/exception_helper.py
+++ decorations/exception_helper.py
@@ -8,8 +8,8 @@ from sqlalchemy.exc import IntegrityError
 
 
 def rollback(session_obj):
-	if session_obj:
-		session_obj.rollback()
+    if session_obj:
+        session_obj.rollback()
 
 
 def handle_exception():
@@ -18,20 +18,20 @@ def handle_exception():
 
         @wraps(f)
         async def decorated_function(request, *args, **kwargs):
-        	session_obj = None
-        	
-        	if 'session_obj' in kwargs:
-        		session_obj = kwargs['session_obj']
+            session_obj = None
+            
+            if 'session_obj' in kwargs:
+                session_obj = kwargs['session_obj']
 
-        	try:
-        		return await f(request, *args, **kwargs)
-        	except IntegrityError:
-        		rollback(session_obj)
-        		return json({
-        			'message': (
-        				'You have to make sure that entity you are creating '
-        				'is unique')
-        			}, 400)
+            try:
+                return await f(request, *args, **kwargs)
+            except IntegrityError:
+                rollback(session_obj)
+                return json({
+                    'message': (
+                        'You have to make sure that entity you are creating '
+                        'is unique')
+                    }, 400)
 
         return decorated_function
 

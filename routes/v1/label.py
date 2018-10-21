c598c60cc919798d48da822374e48d7eb3475914
Edem Tsalah <edem.tsalah@gmail.com>
2018-10-21 20:57:01 +0000

    completed and tests the endpoints for label entity

--- routes/v1/label.py
+++ routes/v1/label.py
@@ -1,9 +1,109 @@
+from datetime import datetime
 from sanic import Blueprint
 from sanic.response import json
 from models.v1.label import Label
+from decorations.authentication_helper import authenticate
+from decorations.session_helper import inject_session
+from decorations.exception_helper import handle_exception
+from helpers import param_helper, query_helper, command_helper
+from helpers import id_helper
 
 label_bp = Blueprint('label_blue_print', url_prefix='/v1/label')
 
-@label_bp.route('/')
-async def label_index(request):
-    return json({'endpoint': 'label blueprint'})
+@label_bp.get('/')
+@inject_session()
+@authenticate()
+@handle_exception()
+async def label_index(request, session_obj, user_obj):
+    params = param_helper.get_json(request)
+    filters = params.get('filters', [])
+    pagination_args = param_helper.get_pagination_details(request)
+    result = query_helper.list_query(
+        session_obj, Label, filters, pagination_args, json_result=True)
+    return json(result)
+
+
+@label_bp.get('/count')
+@inject_session()
+@authenticate()
+@handle_exception()
+async def count_label(request, session_obj, user_obj):
+    params = param_helper.get_json(request)
+    filters = params.get('filters', [])
+    return json(query_helper.count(session_obj, Label, filters))
+
+
+@label_bp.get('/<label_id>')
+@inject_session()
+@authenticate()
+@handle_exception()
+async def find_label(request, session_obj, user_obj, label_id):
+    filters = [
+        {'id': {'$eq': label_id}},
+        {'created_by_id': {'$eq': user_obj['id']}}
+    ]
+    return json(
+        query_helper.find_by_params(
+            session_obj, Label, filters, json_result=True))
+
+
+@label_bp.put('/<label_id>')
+@inject_session()
+@authenticate()
+@handle_exception()
+async def update_label(request, session_obj, user_obj, label_id):
+    params = param_helper.get_json(request)
+    filters = params.get('filters', [])
+    filters.extend([
+        {'id': {'$eq': label_id}},
+        {'created_by_id': {'$eq': user_obj['id']}}
+    ])
+    data = {'ver': id_helper.generate_id(), 'updated_at': datetime.utcnow()}
+    for field in params.keys():
+        if field not in ('id', 'ver', 'created_by_id'):
+            data[field] = params[field]
+    result = command_helper.update_by_params(
+        session_obj, Label, filters, data, json_result=True)
+    session_obj.commit()
+    return json(result)
+
+
+@label_bp.delete('/<label_id>')
+@inject_session()
+@authenticate()
+@handle_exception()
+async def delete_label(request, session_obj, user_obj, label_id):
+    params = param_helper.get_json(request)
+    filters = params.get('filters', [])  # TODO: make sure that ver is passed
+    filters.extend([
+        {'id': {'$eq': label_id}},
+        {'created_by_id': {'$eq': user_obj['id']}}
+    ])
+
+    data = {'ver': id_helper.generate_id(), 'deleted_at': datetime.utcnow()}
+    for field in params.keys():
+        if field not in ('id', 'ver', 'name', 'created_by_id'):
+            data[field] = params[field]
+    result = command_helper.delete_by_params(
+        session_obj, Label, filters, data, json_result=True)
+    session_obj.commit()
+    return json(result)
+
+
+@label_bp.post('/')
+@inject_session()
+@authenticate()
+@handle_exception()
+async def create_label(request, session_obj, user_obj):
+    params = param_helper.get_json(request)
+    _id = id_helper.generate_id()
+    params.update({
+        'created_at': datetime.utcnow(), 'id': _id,
+        'ver': _id, 'created_by_id': user_obj['id']
+    })
+    result = command_helper.save(
+        session_obj, Label, Label.COLUMNS, params, json_result=True)
+
+    session_obj.commit()
+    return json(result)
+

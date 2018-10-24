60518e4908d9589a1007d90212daa1477ea54168
Edem Tsalah <edem.tsalah@gmail.com>
2018-10-24 00:55:09 +0000

    added model and routes for adding labels to a notes and fixed typos on the note_status routes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from routes.v1.status import status_bp as status_bp_v1
 from routes.v1.label import label_bp as label_bp_v1
 from routes.v1.note import note_bp as note_bp_v1
 from routes.v1.note_status import note_status_bp as note_status_bp_v1
+from routes.v1.note_label import note_label_bp as note_label_bp_v1
 from models.v1.base import create_engine
 from models.v1.base import create_db_entities
 from models.v1.base import create_session
@@ -38,7 +39,8 @@ CORS(app)
 
 
 ROUTES = [
-    user_bp_v1, status_bp_v1, label_bp_v1, note_bp_v1, note_status_bp_v1
+    user_bp_v1, status_bp_v1, label_bp_v1, note_bp_v1, note_status_bp_v1,
+    note_label_bp_v1
 ]
 
 for route in ROUTES:
--- routes/v1/note_label.py
+++ routes/v1/note_label.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sanic import Blueprint
 from sanic.response import json
-from models.v1.note_status import NoteStatus
+from models.v1.note_label import NoteLabel
 from decorations.authentication_helper import authenticate
 from decorations.session_helper import inject_session
 from decorations.exception_helper import handle_exception
@@ -9,15 +9,15 @@ from helpers import param_helper, query_helper, command_helper
 from helpers import id_helper
 
 
-note_status_bp = Blueprint(
-	'note_status_blue_print', url_prefix='/v1/note/<note_id>/status')
+note_label_bp = Blueprint(
+	'note_label_blue_print', url_prefix='/v1/note/<note_id>/label')
 
 
-@note_status_bp.get('/')
+@note_label_bp.get('/')
 @inject_session()
 @authenticate()
 @handle_exception()
-async def note_status_index(request, session_obj, user_obj, note_id):
+async def note_label_index(request, session_obj, user_obj, note_id):
     params = param_helper.get_json(request)
     filters = params.get('filters', [])
     filters.extend([
@@ -26,47 +26,47 @@ async def note_status_index(request, session_obj, user_obj, note_id):
 	])
     pagination_args = param_helper.get_pagination_details(request)
     result = query_helper.list_query(
-        session_obj, NoteStatus, filters, pagination_args, json_result=True)
+        session_obj, NoteLabel, filters, pagination_args, json_result=True)
     return json(result)
 
 
-@note_status_bp.get('/count')
+@note_label_bp.get('/count')
 @inject_session()
 @authenticate()
 @handle_exception()
-async def count_note_status(request, session_obj, user_obj, note_id):
+async def count_note_label(request, session_obj, user_obj, note_id):
     params = param_helper.get_json(request)
     filters = params.get('filters', [])
     filters.extend([
     	{'created_by_id': {"$eq": user_obj['id']}},
     	{'note_id': {'$eq': note_id}}
 	])
-    return json(query_helper.count(session_obj, NoteStatus, filters))
+    return json(query_helper.count(session_obj, NoteLabel, filters))
 
 
-@note_status_bp.get('/<status_id>')
+@note_label_bp.get('/<label_id>')
 @inject_session()
 @authenticate()
 @handle_exception()
-async def find_note_status(request, session_obj, user_obj, status_id):
+async def find_note_label(request, session_obj, user_obj, label_id):
     filters = [
-        {'id': {'$eq': status_id}},
+        {'id': {'$eq': label_id}},
         {'created_by_id': {'$eq': user_obj['id']}}
     ]
     return json(
         query_helper.find_by_params(
-            session_obj, NoteStatus, filters, json_result=True))
+            session_obj, NoteLabel, filters, json_result=True))
 
 
-@note_status_bp.put('/<status_id>')
+@note_label_bp.put('/<label_id>')
 @inject_session()
 @authenticate()
 @handle_exception()
-async def update_note_status(request, session_obj, user_obj, status_id):
+async def update_note_label(request, session_obj, user_obj, label_id):
     params = param_helper.get_json(request)
     filters = params.get('filters', [])
     filters.extend([
-        {'id': {'$eq': status_id}},
+        {'id': {'$eq': label_id}},
         {'created_by_id': {'$eq': user_obj['id']}}
     ])
     data = {'ver': id_helper.generate_id(), 'updated_at': datetime.utcnow()}
@@ -74,20 +74,20 @@ async def update_note_status(request, session_obj, user_obj, status_id):
         if field not in ('id', 'ver', 'created_by_id'):
             data[field] = params[field]
     result = command_helper.update_by_params(
-        session_obj, NoteStatus, filters, data, json_result=True)
+        session_obj, NoteLabel, filters, data, json_result=True)
     session_obj.commit()
     return json(result)
 
 
-@note_status_bp.delete('/<status_id>')
+@note_label_bp.delete('/<label_id>')
 @inject_session()
 @authenticate()
 @handle_exception()
-async def delete_note_status(request, session_obj, user_obj, status_id):
+async def delete_note_label(request, session_obj, user_obj, label_id):
     params = param_helper.get_json(request)
     filters = params.get('filters', [])  # TODO: make sure that ver is passed
     filters.extend([
-        {'id': {'$eq': status_id}},
+        {'id': {'$eq': label_id}},
         {'created_by_id': {'$eq': user_obj['id']}}
     ])
 
@@ -96,16 +96,16 @@ async def delete_note_status(request, session_obj, user_obj, status_id):
         if field not in ('id', 'ver', 'name', 'created_by_id'):
             data[field] = params[field]
     result = command_helper.delete_by_params(
-        session_obj, NoteStatus, filters, data, json_result=True)
+        session_obj, NoteLabel, filters, data, json_result=True)
     session_obj.commit()
     return json(result)
 
 
-@note_status_bp.post('/')
+@note_label_bp.post('/')
 @inject_session()
 @authenticate()
 @handle_exception()
-async def create_note_status(request, session_obj, user_obj, note_id):
+async def create_note_label(request, session_obj, user_obj, note_id):
     params = param_helper.get_json(request)
     _id = id_helper.generate_id()
     params.update({
@@ -114,7 +114,7 @@ async def create_note_status(request, session_obj, user_obj, note_id):
         'note_id': note_id
     })
     result = command_helper.save(
-        session_obj, NoteStatus, NoteStatus.COLUMNS, params, json_result=True)
+        session_obj, NoteLabel, NoteLabel.COLUMNS, params, json_result=True)
 
     session_obj.commit()
     return json(result)
